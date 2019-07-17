b569c8e9ecd023562208d7cb462dbdbec5fb75cb
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-07-16 17:54:05 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once(__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . '/vendor/cloudmersive/cloudmersive_virusscan_api_client/vendor/autoload.php');
+
+// Configure API key authorization: Apikey
+$config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKey('Apikey', 'YOUR_API_KEY');
+
+
+
+$apiInstance = new Swagger\Client\Api\ScanApi(
+    
+    
+    new GuzzleHttp\Client(),
+    $config
+);
+$input_file = "/path/to/file.txt"; // \SplFileObject | Input file to perform the operation on.
+
+try {
+    $result = $apiInstance->scanFile($input_file);
+    print_r($result);
+} catch (Exception $e) {
+    echo 'Exception when calling ScanApi->scanFile: ', $e->getMessage(), PHP_EOL;
+}
+?>
